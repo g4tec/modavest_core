1d5ecd34c7de4d1104a636d723cd6c72dbefc087
JailsonLucas <jailson@g4tech.com.br>
2022-09-03 23:21:40 -0300

    fix: adiciona ajuste de propriedades de social network

--- modavest_core/lib/domain/models/enterprise_social_network.dart
+++ modavest_core/lib/domain/models/enterprise_social_network.dart
@@ -1,26 +1,15 @@
-import 'package:equatable/equatable.dart';
-
-class EnterpriseSocialNetwork extends Equatable {
-  final int? sequence;
+class EnterpriseSocialNetwork {
+  int? sequence;
   final num? integrationId;
   final String? address;
   final num? typeCode;
   final String? typeName;
 
-  const EnterpriseSocialNetwork({
+  EnterpriseSocialNetwork({
     this.address,
     this.integrationId,
     this.typeName,
     this.typeCode,
     required this.sequence,
   });
-
-  @override
-  List<Object?> get props => [
-        sequence,
-        integrationId,
-        address,
-        typeCode,
-        typeName,
-      ];
 }

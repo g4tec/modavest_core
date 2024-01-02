12c5d671514884998a3e2b9a0f6338a01d49e78a
JailsonLucas <jailson@g4tech.com.br>
2024-01-02 14:55:24 -0300

    fix: add show discounts on bag official store card

--- lib/widgets/text/modavest_money_bold_text.dart
+++ lib/widgets/text/modavest_money_bold_text.dart
@@ -7,14 +7,16 @@ class ModavestMoneyBoldText extends StatelessWidget {
   final double? discountValue;
   final double fontSize;
   final Color color;
+  final bool showDiscounts;
 
-  const ModavestMoneyBoldText({
-    Key? key,
-    required this.originalValue,
-    this.discountValue,
-    this.fontSize = 24,
-    this.color = Colors.green,
-  }) : super(key: key);
+  const ModavestMoneyBoldText(
+      {Key? key,
+      required this.originalValue,
+      this.discountValue,
+      this.fontSize = 24,
+      this.color = Colors.green,
+      this.showDiscounts = false})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return discountValue == null || (discountValue! - originalValue).abs() < 1
@@ -27,36 +29,50 @@ class ModavestMoneyBoldText extends StatelessWidget {
                 .copyWith(color: color, fontSize: fontSize),
             textAlign: TextAlign.end,
           )
-        : Text.rich(
-            TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                  text: "${toCurrency(originalValue)}\n",
-                  style: discountValue != null && discountValue != originalValue
-                      ? const TextStyle(
-                          color: Colors.grey,
-                          decoration: TextDecoration.lineThrough,
-                        )
-                      : Theme.of(context)
-                          .textTheme
-                          .headline4!
-                          .copyWith(color: color, fontSize: fontSize),
+        : !showDiscounts
+            ? Text(
+                toCurrency(
+                  num.tryParse(
+                        '$discountValue',
+                      )?.toDouble() ??
+                      0,
                 ),
-                if (discountValue != null && discountValue != originalValue)
-                  TextSpan(
-                    text: toCurrency(
-                      num.tryParse(
-                            '$discountValue',
-                          )?.toDouble() ??
-                          0,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4!
+                    .copyWith(color: color, fontSize: fontSize),
+              )
+            : (Text.rich(
+                TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "${toCurrency(originalValue)}\n",
+                      style: discountValue != null &&
+                              discountValue != originalValue
+                          ? const TextStyle(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                            )
+                          : Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(color: color, fontSize: fontSize),
                     ),
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4!
-                        .copyWith(color: color, fontSize: fontSize),
-                  ),
-              ],
-            ),
-          );
+                    if (discountValue != null && discountValue != originalValue)
+                      TextSpan(
+                        text: toCurrency(
+                          num.tryParse(
+                                '$discountValue',
+                              )?.toDouble() ??
+                              0,
+                        ),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4!
+                            .copyWith(color: color, fontSize: fontSize),
+                      ),
+                  ],
+                ),
+              ));
   }
 }

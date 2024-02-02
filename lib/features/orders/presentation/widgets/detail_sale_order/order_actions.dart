cd5b6a9de8ee9102f1087aa744d4323549d7141a
JailsonLucas <jailson@g4tech.com.br>
2024-02-02 11:37:36 -0300

    feat: add new componet of order actions

---
+++ lib/features/orders/presentation/widgets/detail_sale_order/order_actions.dart
@@ -0,0 +1,51 @@
+import 'package:auto_size_text_pk/auto_size_text_pk.dart';
+import 'package:flutter/material.dart';
+import 'package:modavest_core/assets/moda_vest_labels.dart';
+import 'package:modavest_core/widgets/buttons/modavest_button.dart';
+
+class OrderActions extends StatelessWidget {
+  final Function()? onTapToReturn;
+  const OrderActions({
+    super.key,
+    required this.onTapToReturn,
+  });
+
+  Widget buildRow({
+    required String title,
+    required BuildContext context,
+    bool filled = false,
+  }) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      color: filled ? Colors.grey.shade300 : Colors.white30,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: AutoSizeText(
+              title,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        if (onTapToReturn != null)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ModaVestTextButton(
+              title: ModaVestLabels.recreateOrder,
+              onPressed: onTapToReturn,
+              color: Theme.of(context).colorScheme.error,
+            ),
+          ),
+      ],
+    );
+  }
+}

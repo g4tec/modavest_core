040933cef2c3e44bddba185bd62d1183ce87f876
Bruno Dias <bruno.dias@g4tech.com.br>
2026-05-11 09:09:14 -0300

    fix: change to video_player

--- lib/features/products/presentation/widgets/products/details/modavest_video_player.dart
+++ lib/features/products/presentation/widgets/products/details/modavest_video_player.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:appinio_video_player_plus/appinio_video_player_plus.dart';
+// import 'package:appinio_video_player_plus/appinio_video_player_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';

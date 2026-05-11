8c8ed0fda97f6d617f26fab2f4bd2def09a7ad1f
Bruno Dias <bruno.dias@g4tech.com.br>
2026-05-11 08:25:14 -0300

    fix: change to video_player

--- lib/features/products/presentation/widgets/products/details/modavest_video_player.dart
+++ lib/features/products/presentation/widgets/products/details/modavest_video_player.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 import 'package:appinio_video_player_plus/appinio_video_player_plus.dart';
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+import 'package:video_player/video_player.dart';
 
 class ModavestVideoPlayer extends StatefulWidget {
   final String videoUrl;
@@ -14,79 +15,70 @@ class ModavestVideoPlayer extends StatefulWidget {
 }
 
 class _ModavestVideoPlayerState extends State<ModavestVideoPlayer> {
-  CachedVideoPlayerPlus? videoController;
-  CustomVideoPlayerController? _customVideoPlayerController;
+  VideoPlayerController? _controller;
   ValueNotifier<bool> muteNotifier = ValueNotifier<bool>(false);
 
   @override
   void initState() {
-    _initializeVideo();
     super.initState();
+    _initializeVideo();
   }
 
   @override
   void dispose() {
+    _controller?.dispose();
+    muteNotifier.dispose();
     super.dispose();
-    videoController?.dispose();
   }
 
   Future<void> _initializeVideo() async {
-    // final response = await http.get(Uri.parse(widget.videoUrl));
-    // final videoData = response.bodyBytes;
-    // final tempDir = await getTemporaryDirectory();
-    // final videoFile = File('${tempDir.path}/video.mp4');
-    // await videoFile.writeAsBytes(videoData);
-    videoController =
-        CachedVideoPlayerPlus.networkUrl(Uri.parse(widget.videoUrl))
-          ..initialize().then((_) {
-            setState(() {});
-            videoController?.controller.setVolume(0);
-          });
-    _customVideoPlayerController = CustomVideoPlayerController(
-      context: context,
-      videoPlayerController: videoController!,
-    );
+    final response = await http.get(Uri.parse(widget.videoUrl));
+    final videoData = response.bodyBytes;
+    final tempDir = await getTemporaryDirectory();
+    final videoFile = File('${tempDir.path}/video.mp4');
+    await videoFile.writeAsBytes(videoData);
+    _controller = VideoPlayerController.file(videoFile);
+    await _controller!.initialize();
+    await _controller!.setVolume(0);
+    await _controller!.play();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return videoController?.controller.value.isInitialized == true
-        ? Stack(
-            children: [
-              AspectRatio(
-                aspectRatio: videoController!.controller.value.aspectRatio,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  child: CustomVideoPlayer(
-                    customVideoPlayerController: _customVideoPlayerController!,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: IconButton(
-                  onPressed: () {
-                    videoController?.controller
-                        .setVolume(muteNotifier.value ? 0 : 1);
-                    muteNotifier.value = !muteNotifier.value;
-                  },
-                  icon: AnimatedBuilder(
-                    animation: muteNotifier,
-                    builder: ((context, child) {
-                      return Icon(
-                        muteNotifier.value
-                            ? Icons.volume_up
-                            : Icons.volume_mute,
-                        color: Colors.white,
-                      );
-                    }),
-                  ),
-                ),
-              )
-            ],
-          )
-        : const Center(child: CircularProgressIndicator());
+    final controller = _controller;
+
+    if (controller == null || !controller.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: IconButton(
+            onPressed: () {
+              final muted = muteNotifier.value;
+              controller.setVolume(muted ? 0 : 1);
+              muteNotifier.value = !muted;
+            },
+            icon: AnimatedBuilder(
+              animation: muteNotifier,
+              builder: (context, child) {
+                return Icon(
+                  muteNotifier.value ? Icons.volume_up : Icons.volume_mute,
+                  color: Colors.white,
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
